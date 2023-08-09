ccf2057136d73ef84ae64386246e08faab4678cf
priselkov <priselkov@gardium.ru>
2023-08-09 11:23:57 +0300

    App structure

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
+def init_logger() -> None:
+    """Логируем проект."""
+    pass
+
+
+def check_tokens() -> bool:
+    """Проверяет доступность токенов в переменных окружения.
+    Прекращает работу бота если их нет.
+    """
+    pass
+
+
+def read_input() -> str:
+    """Получает сообщение от пользователя."""
+    pass
+
+
+def send_request_to_api_web() -> list[str]:
+    """Посылает пост запрос и получает ответ от api."""
+    pass
+
+
+def check_result_search_tm() -> Dict[str]:
+    """Проверяет поступившие данные и записывает их."""
+    pass
+
+
+def send_message() -> None:
+    """Отправляет пользователю сообщение."""
+    pass
+
+
+def send_data_to_webhook_crm() -> None:
+    """Отправляет данные пользователя в CRM."""
+    pass
+
+
 def main() -> None:
     """Основная логика работы бота."""
     pass
