1e7872265f7b41636fc0388ea4f1d83e56958291
priselkov <priselkov@gardium.ru>
2023-08-09 17:26:55 +0300

    Add token

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+
+
 def init_logger() -> None:
     """Логируем проект."""
     pass
